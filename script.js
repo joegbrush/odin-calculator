c2ec9840df1868990c985a8d2e15ad052af3d831
Joe Brush <joegbrush@gmail.com>
2023-09-01 10:19:55 -0500

    Update script.js, comments

---
+++ script.js
@@ -0,0 +1,8 @@
+// Create add function
+// Create subtract function
+// Create multiply function
+// Create divide function
+// Create variable for each part of operation - number, operator, anotherNumber
+// Create function 'operate' that takes the operator and 2 numbers and calls the function on the numbers
+// Create a basic html calculator with buttons for each number, each operator function and a 'equals' key
+// Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step
