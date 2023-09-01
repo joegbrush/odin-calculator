2f25f72de95c03d75915f7c1f32d63aa1ebfeded
Joe Brush <joegbrush@gmail.com>
2023-09-01 10:23:00 -0500

    Update script.js, add function created

--- script.js
+++ script.js
@@ -6,3 +6,10 @@
 // Create function 'operate' that takes the operator and 2 numbers and calls the function on the numbers
 // Create a basic html calculator with buttons for each number, each operator function and a 'equals' key
 // Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step
+
+// Add function
+function add(number, anotherNumber) {
+  return number + anotherNumber;
+}
+
+console.log(add(1, 2));
