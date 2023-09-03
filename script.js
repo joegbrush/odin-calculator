0698193f6e60fd095779d52a1da9540625ce3a86
Joe Brush <joegbrush@gmail.com>
2023-09-03 09:51:21 -0500

    new js complete not tested

---
+++ script.js
@@ -0,0 +1,85 @@
+let currentTotal = 0;
+let butter = "0";
+
+let previousOperator = null;
+
+const calcScreen = document.querySelector(".calc-numbers");
+
+document
+  .querySelector(".calculator-buttons")
+  .addEventListener("click", function (e) {
+    buttonClick(e.target.innerHTML);
+  });
+
+function buttonClick(value) {
+  if (isNaN(parseInt(value))) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  renderScreen();
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      currentTotal = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer));
+      buffer = "" + currentTotal;
+      previousOperator = null;
+      currentTotal = 0;
+      break;
+    case "&larr;":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    default:
+      handleMath(value);
+      break;
+  }
+}
+
+function handleNumber(value) {
+  if (buffer === "0") {
+    buffer = value;
+  } else {
+    buffer += value;
+  }
+}
+
+function handleMath(value) {
+  const internalBuffer = parseInt(buffer);
+  if (currentTotal === 0) {
+    currentTotal = internalBuffer;
+  } else {
+    flushOperation(internalBuffer);
+  }
+  previousOperator = value;
+  buffer = "0";
+}
+
+function flushOperation(internalBuffer) {
+  if (previousOperator === "+") {
+    currentTotal += internalBuffer;
+  } else if (previousOperator === "-") {
+    currentTotal -= internalBuffer;
+  } else if (previousOperator === "x") {
+    currentTotal *= internalBuffer;
+  } else {
+    currentTotal /= internalBuffer;
+  }
+}
+
+function renderScreen() {
+  calcScreen.value = buffer;
+}
